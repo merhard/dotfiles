6f1eb625b1eafffa9614bb5bc020accd69e9345e
Matthew Erhard <merhard@gmail.com>
2021-10-13 17:47:20 -0400

    Improve install script output

--- install.sh
+++ install.sh
@@ -9,16 +9,22 @@ dotfiles = Dir.glob('*', File::FNM_DOTMATCH, base: __dir__) - excludes
 totals = { symlinked: 0, skipped: 0 }
 
 puts "Symlinking dotfiles to: #{Dir.home}"
+puts
+
+padding = dotfiles.map(&:length).max + 5
 
 dotfiles.each do |dotfile|
   source      = File.join(__dir__, dotfile)
   destination = File.join(Dir.home, dotfile)
 
+  print dotfile.ljust(padding)
+
   if File.exist?(destination)
-    puts "Skipping (already exists): #{destination}"
+    puts "SKIPPED (already exists): #{destination}"
     totals[:skipped] += 1
   else
     FileUtils.symlink(source, destination)
+    puts "SUCCESS"
     totals[:symlinked] += 1
   end
 end
